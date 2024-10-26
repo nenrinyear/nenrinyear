a471c9f98759aecfb80be141c3c02fa8b2a15698
年輪 <nenrin.yearring@gmail.com>
2024-10-26 14:54:38 +0900

    ✏️update nextjs to v15

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 // Trying to : https://github.com/vercel/next.js/discussions/17218#discussioncomment-9686519
 const nextConfig = {
-    experimental: {
-        outputFileTracingIncludes: {
-            "/contents/posts": [process.cwd() + '/contents/posts/**/*'],
-        },
+    outputFileTracingIncludes: {
+        "/contents/posts": [process.cwd() + '/contents/posts/**/*'],
     },
 };
 
