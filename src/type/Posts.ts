db98cd31404d6e6281989408968a88434b39ee1b
年輪 <nenrin.yearring@gmail.com>
2024-11-08 20:16:59 +0900

    🐛Fix date type error

--- src/type/Posts.ts
+++ src/type/Posts.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 const PostSchema = z.object({
     slug: z.string(),
     title: z.string(),
-    date: z.date(),
+    date: z.union([z.string().date(), z.date()]),
     description: z.string(),
     tags: z.array(z.string()).optional(),
     links: z.array(z.object({
